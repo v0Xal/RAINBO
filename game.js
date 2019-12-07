23745d2ead4e0ad99e2db505e1b5955ca48ae8cf
v0Xal <aidenhohoho@gmail.com>
2019-12-07 09:41:22 -0800

    whoo now you can click. OWSCKJASDHKAS

--- game.js
+++ game.js
@@ -1,15 +1,20 @@
-var red;
-var orange;
-var yellow;
-var green;
-var blue;
-var purple;
+var red = 0;
+var orange = 0;
+var yellow = 0;
+var green = 0;
+var blue = 0;
+var purple = 0;
+var pink = 0;
+
+var pressPower = 1;
 
 
 
 
-
-
+function redPlus(){
+  red += pressPower;
+  document.getElementById("red").innerHTML = red;
+}
 
 var last = Date.now()
 var goal = last + 1000;
