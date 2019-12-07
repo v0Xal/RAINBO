eede57417f956595e00556b452c59318236292fa
v0Xal <aidenhohoho@gmail.com>
2019-12-07 09:06:34 -0800

    Fixed some stuff

--- game.js
+++ game.js
@@ -1,1 +1,26 @@
+var red;
+var orange;
+var yellow;
+var green;
+var blue;
+var purple;
 
+
+
+
+
+
+
+var last = Date.now()
+var goal = last + 1000;
+
+function gameLoop () {
+  last = Date.now();
+  if (last >= goal) {
+    goal = goal + 1000;
+
+    document.getElementById("red").innerHTML = red;
+    overflow();
+  }
+}
+setInterval(gameLoop, 1000);
